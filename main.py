a65a04c91e9e4b9a5e7aec9a6d5c03812be1778a
Anastasios Mitsigkolas <tasos1109@gmail.com>
2024-04-16 16:58:52 +0200

    add main function

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ from youtubesearchpython import PlaylistsSearch
 import subprocess
 import torch
 
-# print(torch.cuda.is_available())
+def main ():
+    print(torch.cuda.is_available())
 
 
-# channelsSearch = VideosSearch('Lex Fridman Podcast')
+    # channelsSearch = VideosSearch('Lex Fridman Podcast')
 
-# results = channelsSearch.result()['result']
-# ids = [id['title'] for id in results]
-# print(ids)
-
-
-video_ids = [
-    
-]
+    # results = channelsSearch.result()['result']
+    # ids = [id['title'] for id in results]
+    # print(ids)
 
 
-for id in video_ids:
-    result = subprocess.run("yt-dlp -x --audio-format mp3 -o lexipod/" + id + " -- https://www.youtube.com/watch?v=" + id, shell=True)
-    result = subprocess.run("whisper --language en --device cuda --model medium -o lexipod -- lexipod/" + id + ".mp3", shell=True)
-    print(id + " done")        
+    video_ids = [
+        
+    ]
+
+
+    for id in video_ids:
+        result = subprocess.run("yt-dlp -x --audio-format mp3 -o lexipod/" + id + " -- https://www.youtube.com/watch?v=" + id, shell=True)
+        result = subprocess.run("whisper --language en --device cuda --model medium -o lexipod -- lexipod/" + id + ".mp3", shell=True)
+        print(id + " done")
+
+if __name__ == "__main__":
+    main()
