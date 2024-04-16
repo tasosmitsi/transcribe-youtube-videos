29ec8e98634df4e5d706b60d2451a755cc8fea25
Anastasios Mitsigkolas <tasos1109@gmail.com>
2024-04-16 16:57:31 +0200

    main
    This is a simple code snippet that allows you to search for an entire playlist on youtube retrieve the ids, download the mp3s and transcribe them using whisper medium model.

---
+++ main.py
@@ -0,0 +1,24 @@
+from youtubesearchpython import VideosSearch
+from youtubesearchpython import PlaylistsSearch
+import subprocess
+import torch
+
+# print(torch.cuda.is_available())
+
+
+# channelsSearch = VideosSearch('Lex Fridman Podcast')
+
+# results = channelsSearch.result()['result']
+# ids = [id['title'] for id in results]
+# print(ids)
+
+
+video_ids = [
+    
+]
+
+
+for id in video_ids:
+    result = subprocess.run("yt-dlp -x --audio-format mp3 -o lexipod/" + id + " -- https://www.youtube.com/watch?v=" + id, shell=True)
+    result = subprocess.run("whisper --language en --device cuda --model medium -o lexipod -- lexipod/" + id + ".mp3", shell=True)
+    print(id + " done")        
